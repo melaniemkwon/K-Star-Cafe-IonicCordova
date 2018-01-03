b64d6353691f64e157cd4e35d672615e460abc6b
melaniemkwon <melaniemkwon@gmail.com>
2018-01-03 09:41:26 -0800

    Social Media integration
    -Set up Cordova Social Sharing plugin and corresponding Ionic Native plugin.
    -Configure app to share dish information to Facebook and Twitter account.

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, ToastController, ActionSheetContro
 import { Dish } from '../../shared/dish';
 import { FavoriteProvider } from '../../providers/favorite/favorite'
 import { CommentPage } from '../../pages/comment/comment';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 /**
  * Generated class for the DishdetailPage page.
@@ -29,7 +30,8 @@ export class DishdetailPage {
     private toastCtrl: ToastController,
     private actionSheetCtrl: ActionSheetController,
     private favoriteservice: FavoriteProvider,
-    public modalCtrl: ModalController) {
+    public modalCtrl: ModalController,
+    private socialSharing: SocialSharing) {
       this.dish = navParams.get('dish');
       this.favorite = this.favoriteservice.isFavorite(this.dish.id);
       this.numcomments = this.dish.comments.length;
@@ -67,6 +69,24 @@ export class DishdetailPage {
           text: 'Add a Comment',
           handler: () => {
             this.openComment();
+          }
+        },{
+          text: 'Share via Facebook',
+          handler: () => {
+            this.socialSharing.shareViaFacebook(
+              this.dish.name + ' -- ' + this.dish.description,
+              this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Facebook.'))
+              .catch(() => console.log('Failed to post to Facebook.'));
+          }
+        },{
+          text: 'Share via twitter',
+          handler: () => {
+            this.socialSharing.shareViaTwitter(
+              this.dish.name + ' -- ' + this.dish.description,
+              this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Twitter.'))
+              .catch(() => console.log('Failed to post to Twitter.'));
           }
         },{
           text: 'Cancel',
